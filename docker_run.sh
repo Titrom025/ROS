92c7542bb15ba41fa76b226143d0119114c6356d
Roman Titkov <roman.titkov@altezza.org>
2023-11-28 11:55:58 +0300

    Add cache folder mount into run script

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,7 @@
-docker build -t noetic_full . ; docker run -it --name ros --rm -e DISPLAY=:1 -v /tmp/.X11-unix:/tmp/.X11-unix --gpus all --mount type=bind,source=/home/roman.titkov/ROS/docker_data/,target=/docker_data --mount type=bind,source=/home/roman.titkov/ROS/clip_cache,target=/root/.cache/clip noetic_full
+docker build -t noetic_full . ; \ 
+docker run -it --name ros --rm -e DISPLAY=:1 \
+   -v /tmp/.X11-unix:/tmp/.X11-unix --gpus all \
+   --mount type=bind,source=/home/roman.titkov/ROS/docker_data/,target=/docker_data \
+   --mount type=bind,source=/home/roman.titkov/ROS/cache_clip,target=/root/.cache/clip \
+   --mount type=bind,source=/home/roman.titkov/ROS/cache_torch,target=/root/.torch/iopath_cache/ \
+   noetic_full
